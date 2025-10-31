049725c21a5697bda5750bfef0b129d2063111ac
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-31 13:18:16 +0600

    added the botom section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,17 +4,17 @@
 //       <footer className="bg-[#0e1621] text-white rounded-md">
 //         {/* <!-- Top Section --> */}
 //         <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center px-6 py-10 space-y-8 md:space-y-0 md:space-x-12 relative">
-//           {/* <!-- Contact Us --> */}
-//           <div className="md:w-1/2 flex flex-col items-center md:items-center text-center">
-//             <h3 className="text-lg font-semibold mb-3">CONTACT US</h3>
-//             <p className="text-sm">123 ABC Street, Unit 21, Bangladesh</p>
-//             <p className="text-sm">+88 123456789</p>
-//             <p className="text-sm">Mon - Fri: 09:00 - 22:00</p>
-//             <p className="text-sm">Sat - Sun: 10:00 - 23:00</p>
-//           </div>
+          {/* <!-- Contact Us --> */}
+          <div className="md:w-1/2 flex flex-col items-center md:items-center text-center">
+            <h3 className="text-lg font-semibold mb-3">CONTACT US</h3>
+            <p className="text-sm">123 ABC Street, Unit 21, Bangladesh</p>
+            <p className="text-sm">+88 123456789</p>
+            <p className="text-sm">Mon - Fri: 09:00 - 22:00</p>
+            <p className="text-sm">Sat - Sun: 10:00 - 23:00</p>
+          </div>
 
-//           {/* <!-- Vertical Divider --> */}
-//           <div className="hidden md:block w-[1px] bg-gray-500 h-44"></div>
+          {/* <!-- Vertical Divider --> */}
+          <div className="hidden md:block w-[1px] bg-gray-500 h-44"></div>
 
           {/* <!-- Follow Us --> */}
           <div className="md:w-1/2 flex flex-col items-center md:items-center text-center">
