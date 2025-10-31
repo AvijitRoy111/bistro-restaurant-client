69f495edd9752d52a8b59eb3c6d2bf475eaeb701
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-31 13:16:07 +0600

    added the layout

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,12 +1,12 @@
-// import { Outlet } from "react-router-dom"
-// import Navbar from "../Components/Navbar/Navbar"
-// import Footer from "../Components/Footer/Footer"
+import { Outlet } from "react-router-dom"
+import Navbar from "../Components/Navbar/Navbar"
+import Footer from "../Components/Footer/Footer"
 
 
-// const HomeLayouts = () => {
-//   return (
-//     <div className="container max-w-7xl mx-auto">
-//       <Navbar/>
+const HomeLayouts = () => {
+  return (
+    <div className="container max-w-7xl mx-auto">
+      <Navbar/>
       <div className="container max-w-7xl mx-auto">
           <Outlet></Outlet>
       </div>
