fa6954ff3d621e2459f80923dad10ccb5bcb06bf
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-10-31 13:15:05 +0600

    added the folder

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,18 +1,18 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Components/Navbar/Navbar"
-import Footer from "../Components/Footer/Footer"
+// import { Outlet } from "react-router-dom"
+// import Navbar from "../Components/Navbar/Navbar"
+// import Footer from "../Components/Footer/Footer"
 
 
-const HomeLayouts = () => {
-  return (
-    <div className="container max-w-7xl mx-auto">
-      <Navbar/>
-      <div className="container max-w-7xl mx-auto">
-          <Outlet></Outlet>
-      </div>
-      <Footer/>
-    </div>
-  )
-}
+// const HomeLayouts = () => {
+//   return (
+//     <div className="container max-w-7xl mx-auto">
+//       <Navbar/>
+//       <div className="container max-w-7xl mx-auto">
+//           <Outlet></Outlet>
+//       </div>
+//       <Footer/>
+//     </div>
+//   )
+// }
 
-export default HomeLayouts
+// export default HomeLayouts
