86b608ea9496d9573e59a3b3698b275b02f85e04
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-07 00:08:40 +0600

    added the add to card button

--- src/Pages/Home/ChefRecomended.jsx
+++ src/Pages/Home/ChefRecomended.jsx
@@ -67,15 +67,15 @@
 //               <p className="text-base text-gray-600 mt-3 leading-relaxed">
 //                 {item.description}
 //               </p>
-//               <button className="mt-6 px-5 py-2 bg-gray-200 text-orange-600 font-medium rounded hover:bg-gray-300 transition border-b-4 border-b-orange-900">
-//                 ADD TO CART
-//               </button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+              <button className="mt-6 px-5 py-2 bg-gray-200 text-orange-600 font-medium rounded hover:bg-gray-300 transition border-b-4 border-b-orange-900">
+                ADD TO CART
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default ChefRecomended;
+export default ChefRecomended;
