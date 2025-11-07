cb86bb23f5902ed199aa39e4fc22164d3e1b060e
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-08 00:19:01 +0600

    added the icon

--- src/Pages/DeshBoard/Header/Header.jsx
+++ src/Pages/DeshBoard/Header/Header.jsx
@@ -1,21 +1,21 @@
-import { Clock } from "lucide-react";
+import { Clock, UserRoundCog } from "lucide-react";
 
 const Header = () => {
   const time = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
 
-//   return (
-//     <div className="w-full bg-[#18122B] text-white flex items-center justify-between px-6 py-4 shadow-md">
-//       <h2 className="text-xl font-semibold">Dashboard</h2>
-//       <div className="flex items-center gap-6">
-//         <div className="flex items-center gap-2 text-gray-300">
-//           <Clock size={16} />
-//           <span>{time}</span>
-//         </div>
+  return (
+    <div className="w-full bg-[#18122B] text-white flex items-center justify-between px-6 py-4 shadow-md">
+      <h2 className="text-xl font-semibold">Dashboard</h2>
+      <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2 text-gray-300">
+          <Clock size={16} />
+          <span>{time}</span>
+        </div>
 //         <div className="flex items-center gap-2 bg-[#393053] px-3 py-2 rounded-full">
 //           <div className="w-8 h-8 rounded-full bg-gray-500 flex items-center justify-center font-bold">
 //             GH
 //           </div>
-//           <span className="font-medium">Golap Hasan</span>
+//           <span className="font-medium"><UserRoundCog /></span>
 //         </div>
 //       </div>
 //     </div>
