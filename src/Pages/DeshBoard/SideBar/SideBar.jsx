af9430a4420f789f5c4281e980d1a2458f2e38e1
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-08 00:22:43 +0600

    added the item name

--- src/Pages/DeshBoard/SideBar/SideBar.jsx
+++ src/Pages/DeshBoard/SideBar/SideBar.jsx
@@ -15,21 +15,20 @@ const Sidebar = () => {
   return (
     <div className="w-64 bg-[#18122B] text-white h-screen flex flex-col justify-between">
       <div>
-//         <h2 className="text-2xl font-bold p-6">POPY LIBRARY</h2>
-//         <ul className="px-4 space-y-2">
-//           {menuItems.map((item) => (
-//             <li
-//               key={item.name}
-//               onClick={() => setActive(item.name)}
-//               className={`flex items-center gap-3 p-3 rounded-lg cursor-pointer transition-all ${
-//                 active === item.name
-//                   ? "bg-[#635985] text-white"
-//                   : "hover:bg-[#2a2440]"
-//               }`}
-//             >
-//               {item.icon}
-//               <span>{item.name}</span>
-//             </li>
+        <h2 className="text-2xl font-bold p-6">POPY LIBRARY</h2>
+        <ul className="px-4 space-y-2">
+          {menuItems.map((item) => (
+            <li
+              key={item.name}
+              onClick={() => setActive(item.name)}
+              className={`flex items-center gap-3 p-3 rounded-lg cursor-pointer transition-all ${
+                active === item.name
+                  ? "bg-[#635985] text-white"
+                  : "hover:bg-[#2a2440]"
+              }`}
+            >               {item.icon}
+              <span>{item.name}</span>
+         </li>
 //           ))}
 //         </ul>
 //       </div>
