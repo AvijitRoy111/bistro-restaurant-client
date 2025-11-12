db56921ce15078d55492a822f8a4f7f23ae433fc
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-12 13:59:46 +0600

    added the tab catagory

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -236,17 +236,17 @@
 //         </div>
 //       </div>
 
-//       {/* Tabs */}
-//       <div className="flex justify-center space-x-6 border-b border-gray-200 mb-8">
-//         {categories.map((cat) => (
-//           <button
-//             key={cat}
-//             onClick={() => {
-//               setActive(cat);
-//               setHighlightId(null);
-//               setCurrentPage(1);
-//               setSelectedCategory(cat);
-//             }}
+      {/* Tabs */}
+      <div className="flex justify-center space-x-6 border-b border-gray-200 mb-8">
+        {categories.map((cat) => (
+          <button
+            key={cat}
+            onClick={() => {
+              setActive(cat);
+              setHighlightId(null);
+              setCurrentPage(1);
+              setSelectedCategory(cat);
+            }}
             className={`pb-3 text-sm font-semibold tracking-wide ${active === cat
               ? "text-yellow-500 border-b-2 border-yellow-500"
               : "text-gray-600 hover:text-yellow-500"
