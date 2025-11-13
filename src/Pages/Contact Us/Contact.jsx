b7c60273b42fb6297edb8ca7cab4ae2e1b8d98bd
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-13 13:59:57 +0600

    added the motion

--- src/Pages/Contact Us/Contact.jsx
+++ src/Pages/Contact Us/Contact.jsx
@@ -211,12 +211,12 @@
 //             animate={{ opacity: 1 }}
 //             exit={{ opacity: 0 }}
 //           >
-//             <motion.div
-//               className="bg-white rounded-2xl shadow-xl w-80 text-center p-6 relative"
-//               initial={{ scale: 0.8, opacity: 0 }}
-//               animate={{ scale: 1, opacity: 1 }}
-//               exit={{ scale: 0.8, opacity: 0 }}
-//             >
+            <motion.div
+              className="bg-white rounded-2xl shadow-xl w-80 text-center p-6 relative"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+            >
               <button
                 onClick={() => setShowModal(false)}
                 className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
