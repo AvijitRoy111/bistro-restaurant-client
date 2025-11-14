653f892cf1188ac5c9d8b7d13072f97953ee9308
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-15 00:01:16 +0600

    added the chart

--- src/Pages/DeshBoard/DashBoard/DashBoard.jsx
+++ src/Pages/DeshBoard/DashBoard/DashBoard.jsx
@@ -73,31 +73,31 @@
 //           </ResponsiveContainer>
 //         </div>
 
-        {/* Pie Chart */}
-        <div className="bg-white p-5 rounded-xl shadow-sm flex justify-center items-center">
-          <ResponsiveContainer width="100%" height={250}>
-            <PieChart>
-              <Pie
-                data={pieData}
-                dataKey="value"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                outerRadius={80}
-                label
-              >
-                {pieData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <Legend />
-            </PieChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
+      //   {/* Pie Chart */}
+      //   <div className="bg-white p-5 rounded-xl shadow-sm flex justify-center items-center">
+      //     <ResponsiveContainer width="100%" height={250}>
+      //       <PieChart>
+      //         <Pie
+      //           data={pieData}
+      //           dataKey="value"
+      //           nameKey="name"
+      //           cx="50%"
+      //           cy="50%"
+      //           outerRadius={80}
+      //           label
+      //         >
+      //           {pieData.map((entry, index) => (
+      //             <Cell
+      //               key={`cell-${index}`}
+      //               fill={COLORS[index % COLORS.length]}
+      //             />
+      //           ))}
+      //         </Pie>
+      //         <Legend />
+      //       </PieChart>
+      //     </ResponsiveContainer>
+      //   </div>
+      // </div>
 
      
     </div>
