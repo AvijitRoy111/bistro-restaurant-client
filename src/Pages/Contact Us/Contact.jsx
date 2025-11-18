e89c1fab8bbba9114bb0ba9e2b44c81da3785940
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-18 20:48:36 +0600

    added the phone

--- src/Pages/Contact Us/Contact.jsx
+++ src/Pages/Contact Us/Contact.jsx
@@ -129,16 +129,16 @@
 //               </div>
 //             </div>
 
-//             <div>
-//               <label className="block text-gray-700 mb-2 font-medium">Phone*</label>
-//               <input
-//                 type="number"
-//                 name="number"
-//                 placeholder="Enter your phone number"
-//                 className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#D99904]"
-//                 required
-//               />
-//             </div>
+            <div>
+              <label className="block text-gray-700 mb-2 font-medium">Phone*</label>
+              <input
+                type="number"
+                name="number"
+                placeholder="Enter your phone number"
+                className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#D99904]"
+                required
+              />
+            </div>
 
             <div>
               <label className="block text-gray-700 mb-2 font-medium">Message*</label>
