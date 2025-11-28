c4c977a2571447f5767d5b1a1554de15ad7be3ec
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-28 20:19:33 +0600

    added the text

--- src/Pages/UserCarts/AddReveiw/AddReveiw.jsx
+++ src/Pages/UserCarts/AddReveiw/AddReveiw.jsx
@@ -138,9 +138,9 @@
 //         <div className="fixed inset-0 flex items-center justify-center bg-black/60 z-50">
 //           <div className="bg-white rounded-xl p-8 text-center shadow-lg max-w-sm mx-auto animate-fadeIn">
 //             <FaCheckCircle className="text-green-500 text-5xl mx-auto mb-3" />
-//             <h3 className="text-xl font-bold mb-2 text-gray-800">
-//               Review Submitted Successfully!
-//             </h3>
+            <h3 className="text-xl font-bold mb-2 text-gray-800">
+              Review Submitted Successfully!
+            </h3>
             <p className="text-gray-600 mb-4">
               Thank you for sharing your feedback 
             </p>
