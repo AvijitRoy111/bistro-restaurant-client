13a133e3602a4af594f4ae8f676d31cd65f8a2d9
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-28 20:18:49 +0600

    added the button

--- src/Pages/UserCarts/AddReveiw/AddReveiw.jsx
+++ src/Pages/UserCarts/AddReveiw/AddReveiw.jsx
@@ -144,17 +144,17 @@
 //             <p className="text-gray-600 mb-4">
 //               Thank you for sharing your feedback 
 //             </p>
-//             <button
-//               onClick={() => setSuccess(false)}
-//               className="bg-amber-700 text-white px-4 py-2 rounded-md hover:bg-yellow-700 transition"
-//             >
-//               Close
-//             </button>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+            <button
+              onClick={() => setSuccess(false)}
+              className="bg-amber-700 text-white px-4 py-2 rounded-md hover:bg-yellow-700 transition"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AddReview;
