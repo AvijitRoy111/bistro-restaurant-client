0dd0526201c6fcd0b3dab1fdaaf0bc723bcdb761
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-11-30 22:16:33 +0600

    added the carf

--- src/Pages/UserCarts/UserHome/UserHome.jsx
+++ src/Pages/UserCarts/UserHome/UserHome.jsx
@@ -17,11 +17,11 @@
 //           <FaUtensils className="text-white text-4xl" />
 //         </div>
 
-//         {/* Shop */}
-//         <div className="flex items-center justify-between bg-gradient-to-r from-yellow-300 to-amber-400 rounded-xl p-6 shadow-md">
-//           <div>
-//             <h3 className="text-3xl font-bold text-white">103</h3>
-//             <p className="text-white font-medium">Shop</p>
+        {/* Shop */}
+        <div className="flex items-center justify-between bg-gradient-to-r from-yellow-300 to-amber-400 rounded-xl p-6 shadow-md">
+          <div>
+            <h3 className="text-3xl font-bold text-white">103</h3>
+            <p className="text-white font-medium">Shop</p>
           </div>
           <FaShopify className="text-white text-4xl" />
         </div>
