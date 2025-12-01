76757b7271ad5c17d25848e779fc402f7c034b10
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-01 18:02:48 +0600

    added the motion

--- src/Pages/DeshBoard/AddItems/AddItems.jsx
+++ src/Pages/DeshBoard/AddItems/AddItems.jsx
@@ -170,12 +170,12 @@
 //             animate={{ opacity: 1 }}
 //             exit={{ opacity: 0 }}
 //           >
-//             <motion.div
-//               className="bg-white rounded-2xl shadow-xl w-80 text-center p-6 relative"
-//               initial={{ scale: 0.8, opacity: 0 }}
-//               animate={{ scale: 1, opacity: 1 }}
-//               exit={{ scale: 0.8, opacity: 0 }}
-//             >
+            <motion.div
+              className="bg-white rounded-2xl shadow-xl w-80 text-center p-6 relative"
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+            >
               {/* Close Button */}
               <button
                 onClick={() => setShowModal(false)}
