f8f1a055b0a998210b667b5cac57b212dbfb1ef8
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-01 18:02:10 +0600

    added the icon

--- src/Pages/DeshBoard/AddItems/AddItems.jsx
+++ src/Pages/DeshBoard/AddItems/AddItems.jsx
@@ -180,9 +180,9 @@
 //               <button
 //                 onClick={() => setShowModal(false)}
 //                 className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
-//               >
-//                 <X size={20} />
-//               </button>
+              >
+                <X size={20} />
+              </button>
 
               {/* Success Icon */}
               <div className="flex justify-center mb-3">
