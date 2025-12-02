6daf19eb5733ce820b33fab03145feabf7d5e1a7
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-02 18:26:44 +0600

    added the text

--- src/Pages/DeshBoard/AllContact/AllContact.jsx
+++ src/Pages/DeshBoard/AllContact/AllContact.jsx
@@ -168,7 +168,7 @@
 //       )}
 
 //       {/* Success Modal */}
-//       {showSuccess && (
+      {showSuccess && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
           <div className="bg-green-600 text-white p-6 rounded-xl shadow-lg text-center">
             <h3 className="text-lg font-semibold">Deleted Successfully </h3>
