723e22fcdf2657e2212e1267aad4dc1cd1ebe67d
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-03 18:07:28 +0600

    added the button

--- src/Pages/DeshBoard/AllContact/AllContact.jsx
+++ src/Pages/DeshBoard/AllContact/AllContact.jsx
@@ -40,7 +40,7 @@
 
 //   return (
 //     <div className="max-w-full mx-auto min-h-screen bg-slate-900 text-gray-200">
-//       {/* Header Section */}
+      {/* Header Section */}
       <div className="flex flex-col items-center justify-center space-y-3 pt-10 px-3">
         <p className="text-yellow-500 italic mb-2">---Check it out---</p>
         <hr className="border-2 border-gray-300 w-64 md:w-80" />
