b9a359e36fe11b8f69d138738924c477fb1c5a3f
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-04 22:20:40 +0600

    added the text

--- src/Pages/DeshBoard/AllUser/AllUser.jsx
+++ src/Pages/DeshBoard/AllUser/AllUser.jsx
@@ -184,18 +184,18 @@
 //         </div>
 //       )}
 
-//       {/* Success Modal */}
-//       {showSuccessModal && (
-//         <div className="fixed inset-0 bg-black/60 flex justify-center items-center z-50">
-//           <div className="bg-white rounded-lg shadow-lg p-6 w-[300px] text-center">
-//             <h3 className="text-lg font-semibold text-green-600 mb-2">
-//               User Deleted Successfully!
-//             </h3>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+      {/* Success Modal */}
+      {showSuccessModal && (
+        <div className="fixed inset-0 bg-black/60 flex justify-center items-center z-50">
+          <div className="bg-white rounded-lg shadow-lg p-6 w-[300px] text-center">
+            <h3 className="text-lg font-semibold text-green-600 mb-2">
+              User Deleted Successfully!
+            </h3>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AllUser;
