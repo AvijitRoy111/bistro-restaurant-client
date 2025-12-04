ba7b47bbe91a637cda74db93ac361c2d2a554a52
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-04 22:21:19 +0600

    added the text

--- src/Pages/DeshBoard/AllUser/AllUser.jsx
+++ src/Pages/DeshBoard/AllUser/AllUser.jsx
@@ -173,9 +173,9 @@
 //               >
 //                 Cancel
 //               </button>
-//               <button
-//                 onClick={handleDelete}
-//                 className="bg-amber-700 text-white px-4 py-2 rounded-md hover:bg-amber-800"
+              <button
+                onClick={handleDelete}
+                className="bg-amber-700 text-white px-4 py-2 rounded-md hover:bg-amber-800"
               >
                 Confirm
               </button>
