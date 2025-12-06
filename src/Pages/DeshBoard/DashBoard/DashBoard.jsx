6aee576abf282b5c8f54bc892a4adb668a603f6f
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-06 19:34:30 +0600

    added the icon

--- src/Pages/DeshBoard/DashBoard/DashBoard.jsx
+++ src/Pages/DeshBoard/DashBoard/DashBoard.jsx
@@ -46,7 +46,7 @@
 //         </div>
 //         <div className="bg-gradient-to-r from-yellow-400 to-orange-300 text-white rounded-xl p-5 text-center shadow-md">
 //           <h3 className="text-3xl font-bold">150</h3>
-//           <p className="text-sm mt-1">Customers</p>
+          <p className="text-sm mt-1">Customers</p>
         </div>
         <div className="bg-gradient-to-r from-pink-400 to-red-400 text-white rounded-xl p-5 text-center shadow-md">
           <h3 className="text-3xl font-bold">10</h3>
