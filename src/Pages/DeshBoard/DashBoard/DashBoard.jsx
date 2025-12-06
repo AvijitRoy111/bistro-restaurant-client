cb25f967ad6cf6435ea36178afebb201ac9eb4b6
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-06 19:36:06 +0600

    added the text

--- src/Pages/DeshBoard/DashBoard/DashBoard.jsx
+++ src/Pages/DeshBoard/DashBoard/DashBoard.jsx
@@ -40,7 +40,7 @@
 
 //       {/* Top Cards */}
 //       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-10">
-//         <div className="bg-gradient-to-r from-purple-400 to-pink-400 text-white rounded-xl p-5 text-center shadow-md">
+        <div className="bg-gradient-to-r from-purple-400 to-pink-400 text-white rounded-xl p-5 text-center shadow-md">
           <h3 className="text-3xl font-bold">100</h3>
           <p className="text-sm mt-1">Revenue</p>
         </div>
