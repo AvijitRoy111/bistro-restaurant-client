dc867c2612dfb00cf7f21fb232f5018a611bf07a
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-06 19:35:36 +0600

    added the animation

--- src/Pages/DeshBoard/DashBoard/DashBoard.jsx
+++ src/Pages/DeshBoard/DashBoard/DashBoard.jsx
@@ -42,7 +42,7 @@
 //       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-10">
 //         <div className="bg-gradient-to-r from-purple-400 to-pink-400 text-white rounded-xl p-5 text-center shadow-md">
 //           <h3 className="text-3xl font-bold">100</h3>
-//           <p className="text-sm mt-1">Revenue</p>
+          <p className="text-sm mt-1">Revenue</p>
         </div>
         <div className="bg-gradient-to-r from-yellow-400 to-orange-300 text-white rounded-xl p-5 text-center shadow-md">
           <h3 className="text-3xl font-bold">150</h3>
