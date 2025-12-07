17619aceb7ee28db01c1998886df65110a13badc
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-07 18:56:15 +0600

    added the text

--- src/Pages/Contact Us/ContactBanner.jsx
+++ src/Pages/Contact Us/ContactBanner.jsx
@@ -19,7 +19,7 @@
 //                          bg-black/40 rounded-2xl px-8 py-10 sm:px-12 sm:py-14
 //                          flex flex-col justify-center items-center text-center text-white
 //                          max-w-3xl h-44 md:h-64 w-[90%] space-y-4 backdrop-blur-sm"
-//         >
+        >
           <h2 className="text-3xl sm:text-5xl font-bold mb-3">{title}</h2>
           <p className="text-sm sm:text-lg opacity-90">{subtitle}</p>
         </div>
