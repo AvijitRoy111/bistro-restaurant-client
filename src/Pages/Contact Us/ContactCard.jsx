e0296fec941b268e384505348872c97e07492c3c
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-08 18:40:27 +0600

    added the phone

--- src/Pages/Contact Us/ContactCard.jsx
+++ src/Pages/Contact Us/ContactCard.jsx
@@ -44,8 +44,8 @@
 //         {/* card-3 */}
 //         <div className="w-64 shadow-md border border-gray-100 rounded-b-md overflow-hidden text-center">
 //           <div className="bg-[#D1A054] py-4 flex justify-center items-center">
-//             <Phone className="text-white w-6 h-6" />
-//           </div>
+            <Phone className="text-white w-6 h-6" />
+          </div>
           <div className="bg-gray-200 h-32 py-6 mx-3 mb-3 rounded-b-md">
             <h3 className="text-gray-800 text-lg font-semibold mb-1">
               WORKING HOURS
