9e6ad795dd29ec4055f60b343c068b4972bfb638
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-09 18:25:51 +0600

    added the header

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -6,7 +6,7 @@
 //   return (
 //     <div className="relative min-h-screen flex flex-col">
 //       {/* Fixed Navbar */}
-//       <header className="fixed top-0 left-0 w-full z-50">
+      <header className="fixed top-0 left-0 w-full z-50">
         <Navbar />
       </header>
 
