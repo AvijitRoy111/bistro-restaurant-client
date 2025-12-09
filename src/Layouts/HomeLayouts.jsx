639e60fed934fb7dfeaf36c5c381cca02ddde27a
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-09 18:23:11 +0600

    added the change

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,26 +1,26 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../Components/Navbar/Navbar";
-import Footer from "../Components/Footer/Footer";
+// import { Outlet } from "react-router-dom";
+// import Navbar from "../Components/Navbar/Navbar";
+// import Footer from "../Components/Footer/Footer";
 
-const HomeLayouts = () => {
-  return (
-    <div className="relative min-h-screen flex flex-col">
-      {/* Fixed Navbar */}
-      <header className="fixed top-0 left-0 w-full z-50">
-        <Navbar />
-      </header>
+// const HomeLayouts = () => {
+//   return (
+//     <div className="relative min-h-screen flex flex-col">
+//       {/* Fixed Navbar */}
+//       <header className="fixed top-0 left-0 w-full z-50">
+//         <Navbar />
+//       </header>
 
-      {/* Main Content */}
-      <main className="flex-1 "> 
-        <Outlet />
-      </main>
+//       {/* Main Content */}
+//       <main className="flex-1 "> 
+//         <Outlet />
+//       </main>
 
-      {/* Footer */}
-      <footer className="mt-auto">
-        <Footer />
-      </footer>
-    </div>
-  );
-};
+//       {/* Footer */}
+//       <footer className="mt-auto">
+//         <Footer />
+//       </footer>
+//     </div>
+//   );
+// };
 
-export default HomeLayouts;
+// export default HomeLayouts;
