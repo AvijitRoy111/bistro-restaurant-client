0547dff65fae9987c632dc9897df6cc75df165ed
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-10 18:41:49 +0600

    added the text

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -337,13 +337,13 @@
 //                   </select>
 //                 </div>
 
-//                 <button
-//                   onClick={() => handlePageChange(currentPage + 1)}
-//                   disabled={currentPage === totalPages}
-//                   className="px-3 py-1 text-sm rounded-md border bg-gray-100 hover:bg-amber-700 disabled:opacity-50 hover:text-white"
-//                 >
-//                   Next
-//                 </button>
+                <button
+                  onClick={() => handlePageChange(currentPage + 1)}
+                  disabled={currentPage === totalPages}
+                  className="px-3 py-1 text-sm rounded-md border bg-gray-100 hover:bg-amber-700 disabled:opacity-50 hover:text-white"
+                >
+                  Next
+                </button>
               </div>
             </div>
           )}
