c4fdfeda7a8e6fcaec70a47524d0ab85bfe74572
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-10 18:44:47 +0600

    added the button

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -291,7 +291,7 @@
 //                     className="mt-4 px-4 py-2 bg-gray-200 text-orange-600 font-medium rounded hover:bg-gray-300 transition border-b-4 border-b-orange-900"
 //                   >
 //                     ADD TO CART
-//                   </button>
+                  </button>
                 </div>
               </div>
             ))}
