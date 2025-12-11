d9ffb05e72871b9ce25edeeb1d6387c6b46a6889
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-11 19:14:34 +0600

    added the skleton

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -261,10 +261,10 @@
 //       {loading ? (
 //         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 px-4 pb-20">
 //           {Array(6)
-//             .fill(0)
-//             .map((_, i) => (
-//               <SkeletonCard key={i} />
-//             ))}
+            .fill(0)
+            .map((_, i) => (
+              <SkeletonCard key={i} />
+            ))}
         </div>
       ) : (
         <>
