f8c7e382589294d89ea55bf1052bbba6424f4432
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-13 19:02:54 +0600

    added the text

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -152,7 +152,7 @@
 //     <div className="border rounded-lg shadow-sm p-4 animate-pulse">
 //       <div className="w-full h-48 bg-gray-200 rounded-md mb-4"></div>
 //       <div className="space-y-3 text-center">
-//         <div className="h-5 bg-gray-200 rounded w-3/4 mx-auto"></div>
+        <div className="h-5 bg-gray-200 rounded w-3/4 mx-auto"></div>
         <div className="h-4 bg-gray-200 rounded w-full"></div>
         <div className="h-8 bg-gray-300 rounded w-1/2 mx-auto mt-3"></div>
       </div>
