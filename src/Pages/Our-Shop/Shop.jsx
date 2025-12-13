01d14675774607674201229ad3593ac4a0266911
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-13 19:07:23 +0600

    added the pagination

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -104,10 +104,10 @@
 //       return 0;
 //     });
 
-//   // Pagination
-//   const totalPages = Math.ceil(filtered.length / itemsPerPage || 1);
-//   const startIndex = (currentPage - 1) * itemsPerPage;
-//   const paginatedItems = filtered.slice(startIndex, startIndex + itemsPerPage);
+  // Pagination
+  const totalPages = Math.ceil(filtered.length / itemsPerPage || 1);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const paginatedItems = filtered.slice(startIndex, startIndex + itemsPerPage);
 
   const handlePageChange = (page) => {
     if (page >= 1 && page <= totalPages) setCurrentPage(page);
