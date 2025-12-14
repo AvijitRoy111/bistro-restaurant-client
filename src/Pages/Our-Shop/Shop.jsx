a6d5c3951698817049893d62344124192e8b2f4b
AvijitRoy111 <bidhanbormon24@gmail.com>
2025-12-14 19:21:21 +0600

    added the search

--- src/Pages/Our-Shop/Shop.jsx
+++ src/Pages/Our-Shop/Shop.jsx
@@ -91,7 +91,7 @@
 //   const filtered = filteredByCategory
 //     .filter((item) => {
 //       const query = searchQuery.toLowerCase();
-//       const matchSearch = !query || item.name.toLowerCase().includes(query);
+      const matchSearch = !query || item.name.toLowerCase().includes(query);
       const price = Number(item.price);
       const matchPrice = price >= minPrice && price <= maxPrice;
       return matchSearch && matchPrice;
